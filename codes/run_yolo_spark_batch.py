57f70622b77ea750b03b0b12edf88a15cda29f5b
gy8 <garry.yangguang@gmail.com>
2015-01-22 00:22:37 -0800

    Forgot to add values to it RIP

---
+++ codes/run_yolo_spark_batch.py
@@ -0,0 +1,8 @@
+from spark import SparkContext as sc
+raw = sc.textFile("/guang/dummy.csv") \
+    .map(lambda x: x.split(',')) \
+    .cache()
+
+# average duration grouped by map
+avg_duration_by_map = raw.map(lambda x: (x[5], int(x[10]))) \
+    .reduceByKey(lambda x, y: (x+y)/2).collect()
