2aa0c73024d69753c80c9644ce2201dbe4f3a3f3
gy8 <garry.yangguang@gmail.com>
2015-01-26 22:15:41 -0800

    Update file number for API hitting

--- codes/preprocessing/get_raw_jsons.sh
+++ codes/preprocessing/get_raw_jsons.sh
@@ -2,7 +2,9 @@
 #     @mktime:        1/26/2015
 #     @description:   Making API calls to GGtracker to get simple and extended match details
 
-for match_id in {5720000..5739999}
+# next
+# for match_id in {5710000..5719999}
+for match_id in {5724063..5739999}
 do
   curl -o ../../data/raw/matches_${match_id}_extended.json \
     https://gg2-matchblobs-prod.s3.amazonaws.com/${match_id}
