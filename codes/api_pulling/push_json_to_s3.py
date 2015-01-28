0ac3c19b0d398ed5aada4e14aed413314abf6d47
gy8 <garry.yangguang@gmail.com>
2015-01-27 16:54:55 -0800

    Initialize codes to store raw jsons in S3

---
+++ codes/api_pulling/push_json_to_s3.py
@@ -0,0 +1,52 @@
+"""    @author:            Guang Yang
+       @mktime:            1/27/2015
+       @description:       Connect and write json data to s3
+"""
+
+import os
+import boto
+from boto.s3.key import Key
+
+
+def store_simple(file_name_path, file_name):
+    """ stores simple match details in match_simple bucket on S3
+    """
+
+    k = Key("match_simple")
+    k.key = file_name
+    k.set_contents_from_filename(file_name_path)
+
+
+def store_extended(file_name_path, file_name):
+    """ stores extended match details in match_extended bucket on S3 """
+
+    k = Key("match_extended")
+    k.key = file_name
+    k.set_contents_from_filename(file_name_path)
+
+
+def store_all(file_dir, match_type):
+    """ puts whole directory of simple or extended files into S3
+    """
+
+    matches = os.listdir(file_dir)
+    for match in matches:
+        match_path = "{}/{}".format(file_dir, match)
+        if match_type == "simple":
+            store_simple(match_path, match)
+        elif match_type == "extended":
+            store_extended(match_path, match)
+        else:
+            print("-ruh roh- Unknown match_type, please specify simple or "
+                  "extended.")
+
+
+if __name__ == "__main__":
+    conn = boto.connect_s3()
+
+    current_dir = os.getcwd()
+    match_simple_dir = "{}/../../data/raw/match_simple".format(current_dir)
+    match_extended_dir = "{}/../../data/raw/match_extended".format(current_dir)
+
+    store_all(match_simple_dir, 'simple')
+    # store_all(match_extended_dir, 'extended')
