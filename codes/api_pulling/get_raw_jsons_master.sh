1e9aa6e654bc930b5b0fed0c0353aed9480f3161
gy8 <garry.yangguang@gmail.com>
2015-01-27 18:50:37 -0800

    Amend directory for saving file

--- codes/api_pulling/get_raw_jsons_master.sh
+++ codes/api_pulling/get_raw_jsons_master.sh
@@ -4,10 +4,10 @@
 
 for match_id in {5710000..5719999}
 do
-  curl -o ../data/raw/match_extended/matches_${match_id}_extended.json \
+  curl -o ../../data/raw/match_extended/matches_${match_id}_extended.json \
     https://gg2-matchblobs-prod.s3.amazonaws.com/${match_id}
   sleep 1
-  curl -o ../data/raw/match_simple/matches_${match_id}_simple.json \
+  curl -o ../../data/raw/match_simple/matches_${match_id}_simple.json \
     http://api.ggtracker.com/api/v1/matches/${match_id}.json
   sleep 1
 done
