2ab267174758d08ca827ac2a62b54ea24b36955f
gy8 <garry.yangguang@gmail.com>
2015-01-28 23:09:45 -0800

    Finish serialization for simple details

--- codes/preprocessing/serialize_matches.py
+++ codes/preprocessing/serialize_matches.py
@@ -24,13 +24,21 @@ def get_writer(schema_file, output_file):
 
 
 def serialize_match_simple(schema, output, simple_matches):
-    """ Serialize simple match details from json into an avro file using specified
-    schema
+    """ Serialize simple match details from json into an avro file using
+    specified schema.
+
+    Args:
+        schema:             The avro schema file (avsc) to be used for
+                            serialization.
+        output:             The file path where the resulting serialized avro
+                            file is to be written.
+        simple_matches:     The raw json file to be serialized.
     """
 
     writer = get_writer(schema, output)
     writer.append({
         "match_id": simple_matches['id'],
+        "game_time": 0,
         "replay_id": simple_matches['replays'][0]['id'],
         "replay_url": simple_matches['replays'][0]['url'],
         "release_string": simple_matches['release_string'],
@@ -105,14 +113,34 @@ def serialize_match_simple(schema, output, simple_matches):
     writer.close()
 
 
-def serialize_match_extended(schema, output, extended_matches):
-    """ Serialize extended match details from json into an avro file using
-    specified schema
+def serialize_match_extended_snapshot(schema, output, extended_matches):
+    """ Serialize the 10s snapshots from extended match details from json into
+    timestamped avro files
+
+    Args:
+        schema:             The avro schema file (avsc) to be used for
+                            serialization.
+        output:             The file path where the resulting serialized avro
+                            file is to be written.
+        simple_matches:     The raw json file to be serialized.
+
+
     """
 
-    # writer = get_writer(schema, output)
-    # writer.append({
-    #     "match_id": simple_matches['id'],
+    writer = get_writer(schema, output)
+
+    writer.append({
+        "match_id": extended_matches['id'],
+        "game_time":
+            extended_matches['VespeneCollectionRate'][player0_id][snapshot]
+    })
+
+
+def get_num_snapshot(extended_matches):
+    pass
+
+
+def serialize_match_extended_event(schema, output, extended_matches):
     pass
 
 
@@ -121,14 +149,61 @@ def serialize_matches(source_dir, target_dir, match_type, schema):
     directory (serialized avros)
     """
 
+    matches = os.listdir(source_dir)
+    for match in matches:
+        match_path = "{}/{}".format(source_dir, match)
+
+        # gotta change extension from .json to .avro for target name
+        target_name = "{}.avro".format(os.path.splitext(match)[0])
+        target_path = "{}/{}".format(target_dir, target_name)
+
+        if match_type == "simple":
+            try:
+                match_simple_json = read_json(match_path)
+            except ValueError:
+                print("Skipping {}: file corrupted".format(match))
+
+            try:
+                serialize_match_simple(schema, target_path, match_simple_json)
+                if match_simple_json['game_type'] != "1v1":
+                    print("Skipping {}: not 1v1".format(match))
+                    os.remove(target_path)
+                else:
+                    print("Successfully wrote to {}".format(target_name))
+            except KeyError:
+                print("Skipping {}: no file found".format(match))
+                os.remove(target_path)
+            except TypeError:
+                print("Skipping {}: one player is AI".format(match))
+                os.remove(target_path)
+            except IndexError:
+                print("Skipping {}: one player missing".format(match))
+                os.remove(target_path)
+
+        elif match_type == "extended":
+            match_extended_json = read_json(match_path)
+            serialize_match_extended(schema, target_path, match_extended_json)
+        else:
+            print("-ruh roh- Unknown match_type, please specify simple or "
+                  "extended.")
+
 
 if __name__ == "__main__":
     current_dir = os.getcwd()
-    match_simple_dir = "{}/../../data/raw/match_simple".format(current_dir)
-    match_extended_dir = "{}/../../data/raw/match_extended".format(current_dir)
+    # raw_simple_dir = "{}/../../data/raw/match_simple".format(current_dir)
+    # raw_extended_dir = "{}/../../data/raw/match_extended".format(current_dir)
 
-    serialized_simple_path = "dummy_match_1_simple.avro"
+    # parsed_simple_dir = \
+    #     "{}/../../data/parsed/match_simple".format(current_dir)
+    # parsed_extended_dir = \
+    #     "{}/../../data/parsed/match_extended".format(current_dir)
+
     schema_simple = "schema_match_simple.avsc"
 
-    simple_match = read_json("tests/matches_5720000_simple.json")
-    encode_match_simple(schema_simple, encoded_simple_path, simple_match)
+    yolo_raw_simple_dir = \
+        "{}/../../data/yolo_raw/match_simple".format(current_dir)
+    yolo_parsed_simple_dir = \
+        "{}/../../data/yolo_parsed/match_simple".format(current_dir)
+
+    serialize_matches(yolo_raw_simple_dir, yolo_parsed_simple_dir, 'simple',
+                      schema_simple)
