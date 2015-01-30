0cdb808fc828a3957c0d0e9d73b36c6d04069ce7
gy8 <garry.yangguang@gmail.com>
2015-01-30 02:51:41 -0800

    New spark sql stuff

--- codes/batch/SparkCassie/src/main/scala/test_con_to_cass.scala
+++ codes/batch/SparkCassie/src/main/scala/test_con_to_cass.scala
@@ -1,7 +1,9 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+import org.apache.spark.sql.SQLContext
 import com.datastax.spark.connector._
+import com.databricks.spark.avro._
 
 object SparkCassie {
   def main(args: Array[String]) {
@@ -9,12 +11,14 @@ object SparkCassie {
     //val sc = new SparkContext("spark://54.67.92.5:7077", "all_about_that_database", conf)
 
     val sc = new SparkContext(conf)
-    val rdd = sc.cassandraTable("all_about_that_database", "map_duration")
+    val rdd = sc.cassandraTable("matches_simple", "map_duration_over_time")
 
-    val raw = sc.textFile("/guang/dummy.csv").map(line => line.split(","))
-    val avg_duration = raw.map(line => (line(5), line(10).toInt)).reduceByKey(_+_/2)
+    val sqlContext = new SQLContext(sc)
+    val matches_simple = sqlContext.avroFile("/matches_5673896_simple.avro")
+    matches_simple.registerTempTable("simple")
 
+    val results = sqlContext.sql("select match_id, ended_at, map_name, duration_seconds from simple").collect()
 
-    avg_duration.saveToCassandra("all_about_that_database", "map_duration", SomeColumns("map_name", "duration"))
+    results.saveToCassandra("matches_simple", "map_duration_over_time", SomeColumns("match_id", "ended_at", "map_name", "duration_seconds"))
   }
 }
