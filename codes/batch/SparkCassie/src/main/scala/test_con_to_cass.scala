5c66420085d47196b2725733ffe10f0bf02aa673
gy8 <garry.yangguang@gmail.com>
2015-02-03 19:17:46 +0000

    Get schemaRDD to play nice with saveToCassandra

--- codes/batch/SparkCassie/src/main/scala/test_con_to_cass.scala
+++ codes/batch/SparkCassie/src/main/scala/test_con_to_cass.scala
@@ -8,19 +8,20 @@ import com.databricks.spark.avro._
 object SparkCassie {
   def main(args: Array[String]) {
     val conf = new SparkConf(true).set("spark.cassandra.connection.host", "54.67.42.97")
-    //val sc = new SparkContext("spark://54.67.92.5:7077", "all_about_that_database", conf)
 
     val sc = new SparkContext(conf)
-    val rdd = sc.cassandraTable("matches_simple", "map_duration_over_time")
+    val cassie = sc.cassandraTable("matches_simple", "map_duration_over_time")
+    // val cassie = sc.cassandraTable("matches_simple", "yolo_try")
 
+    // load avro files and save as table
     val sqlContext = new SQLContext(sc)
     val matches_simple = sqlContext.avroFile("/matches_5673896_simple.avro")
     matches_simple.registerTempTable("simple")
 
-    // val results = sqlContext.sql("select match_id, ended_at, map_name, duration_seconds from simple")
-    val results = sqlContext.sql("select match_id from simple").map(line => line.toSeq)
-
-    // results.saveToCassandra("matches_simple", "map_duration_over_time", SomeColumns("match_id", "ended_at", "map_name", "duration_seconds"))
-    results.saveToCassandra("matches_simple", "map_duration_over_time", SomeColumns("match_id"))
+    // make queries get data
+    // val results = sqlContext.sql("insert into map_duration_over_time.matches_simple select match_id, ended_at, map_name, duration_seconds from simple")
+    val query_results  = sqlContext.sql("select match_id, ended_at, map_name, duration_seconds from simple")
+    val results = query_results.map(row => (row.getInt(0), row.getFloat(1), row.getString(2), row.getInt(3)))
+    results.saveToCassandra("matches_simple", "map_duration_over_time", SomeColumns("match_id", "ended_at", "map_name", "duration_seconds"))
   }
 }
