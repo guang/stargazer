f49a55abdf34905f886253cbb112c108dfa80fe8
gy8 <garry.yangguang@gmail.com>
2015-02-06 10:34:55 +0000

    Add script to get top 10 maps

--- codes/batch/SparkCassie/src/main/scala/test_con_to_cass.scala
+++ codes/batch/SparkCassie/src/main/scala/test_con_to_cass.scala
@@ -24,18 +24,17 @@ object SparkCassie {
 //    results.saveToCassandra("matches_simple", "map_duration_over_time",
 //      SomeColumns("match_id", "ended_at", "map_name", "duration_seconds"))
 
-    // -- Duration Over Time by Day
-    val duration_over_time = sqlContext.sql("SELECT match_id, ended_at, map_name, " +
-      "duration_seconds FROM simple")
-    // WHAT IS GOING ON OMG HOW DO U DO THIS
-    val duration_over_time_day = duration_over_time.map(row => (row.getInt(0),
-      (math floor row.getFloat(1)/24/3600)*24*3600, row.getString(2), row.getInt(3)))
-    val duration_over_time_day_mapped = duration_over_time_day.map(row => ((row._2, row._3),
-      row._4)).mapValues(x => (x,1)).reduceByKey((x,y) => (x._1 + y._1, x._2 + y._2))
-    val results = duration_over_time_day_mapped.map(row => (row._1._1, row._1._2,
-      row._2._1.asInstanceOf[Float]/row._2._2.asInstanceOf[Float]/60))
-    results.saveToCassandra("matches_simple", "yolo_duration_all_day",
-      SomeColumns("ended_at", "map_name", "duration_minutes"))
+//     // -- Duration Over Time by Day
+//     val duration_over_time = sqlContext.sql("SELECT match_id, ended_at, map_name, " +
+//       "duration_seconds FROM simple")
+//     val duration_over_time_day = duration_over_time.map(row => (row.getInt(0),
+//       (math floor row.getFloat(1)/24/3600)*24*3600, row.getString(2), row.getInt(3)))
+//     val duration_over_time_day_mapped = duration_over_time_day.map(row => ((row._2, row._3),
+//       row._4)).mapValues(x => (x,1)).reduceByKey((x,y) => (x._1 + y._1, x._2 + y._2))
+//     val results = duration_over_time_day_mapped.map(row => (row._1._1, row._1._2,
+//       row._2._1.asInstanceOf[Float]/row._2._2.asInstanceOf[Float]/60))
+//     results.saveToCassandra("matches_simple", "yolo_duration_all_day",
+//       SomeColumns("ended_at", "map_name", "duration_minutes"))
 
 //     // -- Duration Over Time By Expansion --
 //     val query_results = sqlContext.sql("SELECT match_id, ended_at, map_name, duration_seconds FROM simple WHERE player0_current_league_1v1 = 3")
@@ -44,8 +43,15 @@ object SparkCassie {
 //     // FUCK HOW DO I DO THIS
 //     val query_results = sqlContext.sql("SELECT match_id, ended_at, map_name, duration_seconds FROM simple WHERE  = 3")
 // 
-//     // -- Over Time --
-// 
+
+    // -- Top 10 Maps --
+    // TODO: add url
+    val top_10 = sqlContext.sql("SELECT map_name, count(map_name) as c FROM simple GROUP BY map_name ORDER BY c DESC LIMIT 10")
+    val results = top_10.map(row => (row.getString(0), row.getLong(1)))
+    results.saveToCassandra("matches_simple", "yolo_top_10",
+      SomeColumns("map_name", "match_count"))
+
+
 // 
 //     // -- AUX --
 //     // -- URL for Map --
