21acee83710f0e72f83ee3bc8d9372bbb5a8f415
gy8 <garry.yangguang@gmail.com>
2015-02-06 11:44:12 +0000

    Consolidate api pulling scripts into one

--- codes/api_pulling/get_raw_jsons.sh
+++ codes/api_pulling/get_raw_jsons.sh
@@ -2,12 +2,41 @@
 #     @mktime:        1/26/2015
 #     @description:   Making API calls to GGtracker to get simple and extended match details
 
-for match_id in {5724063..5739999}
-do
-  curl -o ../../data/raw/match_extended/matches_${match_id}_extended.json \
-    https://gg2-matchblobs-prod.s3.amazonaws.com/${match_id}
-  sleep 1
-  curl -o ../../data/raw/match_simple/matches_${match_id}_simple.json \
-    http://api.ggtracker.com/api/v1/matches/${match_id}.json
-  sleep 1
-done
+get_json_simple ( ) {
+  curl -o ../../data/raw/match_simple/matches_${1}_simple.json \
+    http://api.ggtracker.com/api/v1/matches/${1}.json
+}
+my_ip=$(curl ident.me)
+
+
+if [ "$my_ip" = "54.67.92.5" ]; then
+  # master 
+  for match_id in {1..5680000..100}
+  do
+    get_json_simple $match_id
+    sleep 1
+  done
+elif [ "$my_ip" = "54.67.61.99" ]; then
+  # brant
+  for match_id in {2..5680000..100}
+  do
+    get_json_simple $match_id
+    sleep 1
+  done
+elif [ "$my_ip" = "54.67.63.16" ]; then
+  # cj
+  for match_id in {3..5680000..100}
+  do
+    get_json_simple $match_id
+    sleep 1
+  done
+elif [ "$my_ip" = "54.183.89.126" ]; then
+  # ed
+  for match_id in {4..5680000..100}
+  do
+    get_json_simple $match_id
+    sleep 1
+  done
+else
+  echo "Invalid public IP :(("
+fi
