491a56ee2036e66606e1cacd9065082ea7c9bf24
gy8 <garry.yangguang@gmail.com>
2015-02-11 23:19:24 +0000

    Update kafka producer to produce the correct messages

--- codes/ingestion/test_kafka_producer.py
+++ codes/ingestion/test_kafka_producer.py
@@ -4,17 +4,19 @@
                                 puts in kafka
 """
 
+import json
 import requests
 from kafka import *
 import time
 
 
-def send_messages_to_kafka():
+def send_messages_to_kafka(kafka, producer):
     try:
         recent_matches = requests.get("http://api.ggtracker.com/api/v1/matches")
-        recent_matches_json = recent_matches_raw.json()['collection']
+        recent_matches_json = recent_matches.json()['collection']
 
-        for recent_match in recent_matches:
+        for recent_match in recent_matches_json:
+            print((recent_match['map_name'], recent_match['ended_at']))
             producer.send_messages(KAFKA_TOPIC, json.dumps(recent_match))
 
         time.sleep(SLEEP_TIMER)
@@ -24,17 +26,18 @@ def send_messages_to_kafka():
         kafka.close()
 
 
-i = 0
-while True:
-    i = i + 1
-    producer.send_messages("test", str(i))
-end
+# i = 0
+# while True:
+#     i = i + 1
+#     producer.send_messages("test", str(i))
+# end
 
 
 if __name__ == "__main__":
     my_kafka = KafkaClient("ec2-54-67-92-5.us-west-1.compute.amazonaws.com:"
                            "9092")
-    producer = SimpleProducer(my_kafka)
-    SLEEP_TIMER = 1         # in seconds
+    my_producer = SimpleProducer(my_kafka)
+    SLEEP_TIMER = 5         # in seconds
     KAFKA_TOPIC = "recent_matches"
-    send_messages_to_kafka()
+    while True:
+      send_messages_to_kafka(my_kafka, my_producer)
