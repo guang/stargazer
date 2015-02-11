42d6a96802ee92b187f751e25a41674b2861945c
gy8 <garry.yangguang@gmail.com>
2015-02-10 23:56:29 -0800

    Add streaming codes to get most recent maps

--- codes/streaming/StreamProcessing/src/main/scala/recent_maps.scala
+++ codes/streaming/StreamProcessing/src/main/scala/recent_maps.scala
@@ -18,36 +18,20 @@ object StreamExample {
     val conf = new SparkConf().set("spark.cassandra.connection.host", "54.67.42.97")
     val ssc = new StreamingContext(conf, Seconds(5))
 
-//    val zkQuorum = "54.67.43.21:2181"
-//    val groupID = "rt"
-//    val topics = Map("messages" -> 1)
-//    val kafkaStream = KafkaUtils.createStream(ssc, zkQuorum, groupID, topics)
-//
-//    def single_to_double(digit: String): String = if (digit.length==1) "0"+digit else digit
-//
-//
-//    val parsed_message = kafkaStream.map( x => parse(x._2) ).cache()
-//
-//    val message_counts_by_county = parsed_message.map( message => (compact(render( message \ "state" )) + "," + compact(render( message \ "county" )), 1) )
-//    val reduced_message_counts_by_county = message_counts_by_county.reduceByKey( _ + _ )
-//    val tup_message_counts_by_county = reduced_message_counts_by_county.map( tup => {val state_county = tup._1.split(",")
-//      (state_county(0).tail.dropRight(1), state_county(1).tail.dropRight(1), tup._2)})
-//
-//    tup_message_counts_by_county.saveToCassandra("puppy", "by_county_rt", SomeColumns("state", "county", "count") )
-//
-//
-//    val messages_by_county = parsed_message.map( message => {val date_array = compact(message \ "timestamp").tail.dropRight(1).split(",")
-//      (compact(render( message \ "state" )).tail.dropRight(1),
-//       compact(render( message \ "county" )).tail.dropRight(1),
-//      (date_array(0) + single_to_double(date_array(1)) + single_to_double(date_array(2))).toInt,
-//      (single_to_double(date_array(3)) + single_to_double(date_array(4)) + single_to_double(date_array(5))).toInt,
-//       compact(render( message \ "messageID" )).toInt,
-//       message.toString )}
-//         )
-//
-//    messages_by_county.saveToCassandra("puppy", "by_couny_msgs", SomeColumns("state", "county", "date", "time", "messageid", "message") )
-//
-//    ssc.start()
-//    ssc.awaitTermination()
+    val zk_quorum = "localhost:2181"
+    val group_id = "spark_streaming"
+    val topics = Map("recent_matches" -> 1)
+    val kafka_stream = KafkaUtils.createStream(ssc, zk_quorum, group_id, topics)
+
+    val parsed_message = kafkaStream.map( x => parse(x._2) ).cache()
+
+    val recent_maps = parsed_message.map( message => (compact(render( message \ "map_name" )),
+      compact(render( message \ "ended_at" ))))
+
+    recent_maps.saveToCassandra("matches_simple", "yolo_recent_10",
+      SomeColumns("map_name", "ended_at"))
+
+    ssc.start()
+    ssc.awaitTermination()
   }
 }
