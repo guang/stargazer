4819c39a18e5fec5c01d2cdfe421dfec7bfe096c
gy8 <garry.yangguang@gmail.com>
2015-02-12 10:51:37 -0800

    Clean up file naming for batching

--- codes/batch/run_yolo_spark_batch.py
+++
@@ -1,9 +0,0 @@
-from pyspark import SparkContext
-sc = SparkContext()
-raw = sc.textFile("/guang/dummy.csv") \
-    .map(lambda x: x.split(',')) \
-    .cache()
-
-# average duration grouped by map
-avg_duration_by_map = raw.map(lambda x: (x[5], int(x[10]))) \
-    .reduceByKey(lambda x, y: (x+y)/2).collect()
